e0481127357613d904a9caa886f4baab68fcfc7e
IgorN <Igor.nikityuk@gmail.com>
2015-09-29 00:36:59 +0300

    new style for app

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'devise'
 
 gem 'puma'
 
+gem "paperclip", "~> 4.3"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814150829) do
+ActiveRecord::Schema.define(version: 20150924142730) do
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "text",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "title",               limit: 255
+    t.text     "text",                limit: 65535
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.string   "avatar_file_name",    limit: 255
+    t.string   "avatar_content_type", limit: 255
+    t.integer  "avatar_file_size",    limit: 4
+    t.datetime "avatar_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
